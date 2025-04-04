d5e9fe98c6e90a6839875b88b6c5bbc403a4ae63
LeandroLDomingos <leandrolucasdomingos15@outlook.com>
2025-04-04 10:25:42 -0300

    fix: enum users status

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
             $table->string('city')->nullable();
             $table->string('state')->nullable();
             $table->string('country')->nullable();
-            $table->enum('status', ['active', 'inactive', 'pending', 'blocked']);
+            $table->enum('status', ['active', 'inactive', 'pending','is_analising', 'blocked']);
             $table->timestamp('approved_at')->nullable()->after('status');
             $table->string('identity_document')->nullable()->after('approved_at');
             $table->string('residence_proof')->nullable()->after('identity_document');
